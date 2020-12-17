1253e957539e67fae52f58f8b3bb449a25054cf3
Andrey Chistyakov <anchisty@microsoft.com>
2020-12-17 12:28:13 +0000

    SavedGameManager moved to the project

---
+++ pipes/Source/HoloPipes/Private/SaveGameLoader.cpp
@@ -0,0 +1,87 @@
+#include "pch.h"
+#include "SavedGameLoader.h"
+
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Streams;
+using namespace winrt::Windows::Foundation;
+
+SavedGameLoader::SavedGameLoader(SavedGamePathType type, PCWSTR name)
+{
+    m_name = name;
+    m_pathType = type;
+
+    Start();
+}
+
+SavedGameLoader::~SavedGameLoader()
+{
+    WaitForCompletion();
+}
+
+IFACEMETHODIMP SavedGameLoader::GetBytes(_Outptr_ const byte** ppBytes, _Out_ UINT32* pCount)
+{
+    (*ppBytes) = nullptr;
+    (*pCount) = 0;
+    HRESULT hr = E_UNEXPECTED;
+
+    if (m_state == SavedGameState::Complete)
+    {
+        (*ppBytes) = m_bytes.data();
+        (*pCount) = static_cast<UINT32>(m_bytes.size());
+        hr = S_OK;
+    }
+
+    return hr;
+}
+
+IFACEMETHODIMP_(void) SavedGameLoader::WaitForCompletion()
+{
+    if (m_thread.joinable())
+    {
+        m_thread.join();
+    }
+}
+
+void SavedGameLoader::Start()
+{
+    // Unreal's game thread is an STA, which means any async action we start on that
+    // thread will only execute in the STA. As such, we can't wait on the async action
+    // to complete on that thread without causing a deadlock. To deal with that problem,
+    // we launch a seperate MTA thread which in turn launches the async action and
+    // waits for it to complete. If we need to ensure the action is completed, we can
+    // simply join our MTA thread.
+    std::thread thread([this]() { RunThread(); });
+    m_thread = std::move(thread);
+}
+
+void SavedGameLoader::RunThread()
+{
+    winrt::init_apartment();
+    Run().get();
+}
+
+IAsyncAction SavedGameLoader::Run()
+{
+    SavedGameState result = SavedGameState::Failed;
+
+    try
+    {
+        StorageFolder folder = ResolvePath(m_pathType, false);
+        StorageFile file = co_await folder.GetFileAsync(m_name);
+
+        if (file)
+        {
+            IBuffer buffer = co_await FileIO::ReadBufferAsync(file);
+            if (buffer)
+            {
+                m_bytes.assign(buffer.data(), buffer.data() + buffer.Length());
+                result = SavedGameState::Complete;
+            }
+        }
+    }
+    catch (...)
+    {
+    }
+
+    m_state = result;
+}
---
+++ pipes/Source/HoloPipes/Private/SavedGameSaver.cpp
@@ -0,0 +1,86 @@
+#include "pch.h"
+#include "SavedGameSaver.h"
+
+using namespace winrt::Windows::Storage;
+using namespace winrt::Windows::Storage::Streams;
+using namespace winrt::Windows::Foundation;
+
+SavedGameSaver::SavedGameSaver(SavedGamePathType type,
+                               PCWSTR name,
+                               _In_ const byte* pBytes,
+                               UINT32 count)
+{
+    m_bytes.assign(pBytes, pBytes + count);
+    m_name = name;
+    m_pathType = type;
+
+    Start();
+}
+
+SavedGameSaver::~SavedGameSaver()
+{
+    WaitForCompletion();
+}
+
+IFACEMETHODIMP SavedGameSaver::GetBytes(_Outptr_ const byte** ppBytes, _Out_ UINT32* pCount)
+{
+    (*ppBytes) = m_bytes.data();
+    (*pCount) = static_cast<UINT32>(m_bytes.size());
+    return S_OK;
+}
+
+IFACEMETHODIMP_(void) SavedGameSaver::WaitForCompletion()
+{
+    if (m_thread.joinable())
+    {
+        m_thread.join();
+    }
+}
+
+void SavedGameSaver::Start()
+{
+    // Unreal's game thread is an STA, which means any async action we start on that
+    // thread will only execute in the STA. As such, we can't wait on the async action
+    // to complete on that thread without causing a deadlock. To deal with that problem,
+    // we launch a seperate MTA thread which in turn launches the async action and
+    // waits for it to complete. If we need to ensure the action is completed, we can
+    // simply join our MTA thread.
+    std::thread thread([this]() { RunThread(); });
+    m_thread = std::move(thread);
+}
+
+void SavedGameSaver::RunThread()
+{
+    winrt::init_apartment();
+    Run().get();
+}
+
+IAsyncAction SavedGameSaver::Run()
+{
+    SavedGameState result = SavedGameState::Failed;
+
+    try
+    {
+        std::wstring tempFileName = m_name + L".tmp";
+
+        StorageFolder tempFolder = ResolvePath(m_pathType, true);
+
+        StorageFile tempFile = co_await tempFolder.CreateFileAsync(tempFileName, CreationCollisionOption::GenerateUniqueName);
+
+        if (tempFile)
+        {
+            co_await FileIO::WriteBytesAsync(tempFile, m_bytes);
+
+            StorageFolder destFolder = ResolvePath(m_pathType, false);
+            co_await tempFile.MoveAsync(destFolder, m_name, NameCollisionOption::ReplaceExisting);
+            result = SavedGameState::Complete;
+        }
+    }
+    catch (...)
+    {
+    }
+
+    m_state = result;
+}
+
+
